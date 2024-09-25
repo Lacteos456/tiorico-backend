4c11c57b34af3404da37b71b697c9fe87331e151
Lacteos456 <lacteostio1213@gmail.com>
2024-09-25 12:21:12 -0500

    Agregar controladores de productos y categorias

--- src/main/java/com/tiorico/apptiorico/controllers/CategoryController.java
+++ src/main/java/com/tiorico/apptiorico/controllers/CategoryController.java
@@ -1,13 +1,39 @@
 package com.tiorico.apptiorico.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.tiorico.apptiorico.dtos.CategoryDTO;
+import com.tiorico.apptiorico.mappers.CategoryMapper;
+import com.tiorico.apptiorico.models.Category;
+import com.tiorico.apptiorico.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/categories")
 @CrossOrigin("*")
 public class CategoryController
 {
+    @Autowired
+    private CategoryService categoryService;
 
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    @GetMapping("/")
+    public ResponseEntity<List<CategoryDTO>> getAllCategories() {
+        List<Category> categories = categoryService.findAll();
+        List<CategoryDTO> categoryDTOs = categories.stream()
+                .map(categoryMapper::toDTO)
+                .toList();
+        return ResponseEntity.ok(categoryDTOs);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<CategoryDTO> createCategory(@RequestBody CategoryDTO categoryDTO) {
+        Category category = categoryMapper.toEntity(categoryDTO);
+        Category savedCategory = categoryService.save(category);
+        return ResponseEntity.ok(categoryMapper.toDTO(savedCategory));
+    }
 }
--- src/main/java/com/tiorico/apptiorico/controllers/ProductController.java
+++ src/main/java/com/tiorico/apptiorico/controllers/ProductController.java
@@ -1,13 +1,39 @@
 package com.tiorico.apptiorico.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.tiorico.apptiorico.dtos.ProductDTO;
+import com.tiorico.apptiorico.mappers.ProductMapper;
+import com.tiorico.apptiorico.models.Product;
+import com.tiorico.apptiorico.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/products")
 @CrossOrigin("*")
 public class ProductController
 {
+    @Autowired
+    private ProductService productService;
 
+    @Autowired
+    private ProductMapper productMapper;
+
+    @GetMapping("/")
+    public ResponseEntity<List<ProductDTO>> getAllProducts() {
+        List<Product> products = productService.findAll();
+        List<ProductDTO> productDTOs = products.stream()
+                .map(productMapper::toDTO)
+                .toList();
+        return ResponseEntity.ok(productDTOs);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO productDTO) {
+        Product product = productMapper.toEntity(productDTO);
+        Product savedProduct = productService.save(product);
+        return ResponseEntity.ok(productMapper.toDTO(savedProduct));
+    }
 }
